131ed070f8019e0a9595a2c387836bc0a6829974
Simon <70409292+pigupp@users.noreply.github.com>
2023-02-12 02:06:26 +0100

    Update video.js

--- docs/video.js
+++ docs/video.js
@@ -1,10 +1,10 @@
 // - Config -
 
 // List of all available videos (e.g. '['video1.mp4', 'video2.ogg', 'video3.mov']')
-const items = ['a.mp4', 'b.mp4', 'c.mp4']
+const items = ['https://img.ifunny.co/videos/55bb2b9637b70f249bf5cad5bb5bd399da1f13fc1da3fc2acfd16a5b5d7d0f73_1.mp4', 'b.mp4', 'c.mp4']
 
 // Folder path (e.g. 'videos' or 'folder1/folder2')
-const folder = 'videos'
+const folder = ''
 
 // Redirects to video (Autoplay with sound & video-controls, but will not loop & fit to viewport)
 // Settings below this one will be ignored if set to true
@@ -26,7 +26,7 @@ const loop = true
 // Set variables
 var attributes = ""
 var file = items[Math.floor(Math.random() * items.length)]
-var path = folder + "/" + file
+var path = folder + file
 
 // Apply settings
 if (autoplay) { attributes += " autoplay muted" }
